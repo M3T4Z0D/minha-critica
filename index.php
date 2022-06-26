2722d44f9951782ccc3177e2a9debb6cef70a918
Thiago Bauce <thiedbauce@gmail.com>
2022-06-26 16:33:41 -0400

    buscando diretamente das variáveis

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +8,24 @@
     <link rel="stylesheet" href="src/public/css/navbar.css">
     <link href="src/public/css/home.css" rel="stylesheet">
 </head>
+
 <body>
     <nav>
-        <object width="100%"  height="100%" data="nav__menu.php"></object>
+        <object width="100%" height="100%" data="../../nav__menu.php"></object>
     </nav>
-    <div class = "corpo">
-        <div class ="movies">
+    <div class="corpo">
+        <div class="movies">
             <div class="movies-quad">
                 <div class="img">
-                    <img src="src/public/img/hp3poster.jpg" />
+                    <img src=<?= $filmes->caminhoimg ?> />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Harry Potter e o Prisioneiro de Askaban</div>
-                    <div class="ano">Ano: 2004</div>
-                    <div class="duracao">Duração: 2h 20min</div>
-                    <div class="gênero">Gênero: Fantasia, Aventura</div>
-                    <div class="elenco">Elenco: Daniel Radcliffe, Rupert Grint, Emma Watson</div>
-                    <div class="avaliacao">Avaliação: 4.3</div>
+                    <div class="titulo">Título: <?= $filmes->titulo ?></div>
+                    <div class="ano">Ano: <?= $filmes->ano ?></div>
+                    <div class="duracao">Duração: <?= $filmes->duracao ?></div>
+                    <div class="gênero">Gênero: <?= $filmes->genero ?></div>
+                    <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
+                    <div class="avaliacao">Avaliação: <?= $filmes->avaliacao ?></div>
                 </div>
             </div>
             <div class="movies-quad">
@@ -31,12 +33,12 @@
                     <img src="src/public/img/interestelar.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Interestelar</div>
-                    <div class="ano">Ano: 2014</div>
-                    <div class="duracao">Duração: 2h 49min</div>
-                    <div class="gênero">Gênero: Ficção científica, Drama</div>
-                    <div class="elenco">Elenco: Matthew McConaughey, Anne Hathaway, Michael Caine</div>
-                    <div class="avaliacao">Avaliação: 4.7</div>
+                    <div class="titulo">Título: <?= $filmes->titulo ?></div>
+                    <div class="ano">Ano: <?= $filmes->ano ?></div>
+                    <div class="duracao">Duração: <?= $filmes->duracao ?></div>
+                    <div class="gênero">Gênero: <?= $filmes->genero ?></div>
+                    <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
+                    <div class="avaliacao">Avaliação: <?= $filmes->avaliacao ?></div>
                 </div>
             </div>
             <div class="movies-quad">
@@ -44,12 +46,12 @@
                     <img src="src/public/img/avengersinfwar.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Vingadores: Guerra Infinita</div>
-                    <div class="ano">Ano: 2018</div>
-                    <div class="duracao">Duração: 2h 36min</div>
-                    <div class="gênero">Gênero: Aventura, Ação, Fantasia</div>
-                    <div class="elenco">Elenco: Robert Downey Jr., Chris Hemsworth, Mark Ruffalo</div>
-                    <div class="avaliacao">Avaliação: 4.2</div>
+                    <div class="titulo">Título: <?= $filmes->titulo ?></div>
+                    <div class="ano">Ano: <?= $filmes->ano ?></div>
+                    <div class="duracao">Duração: <?= $filmes->duracao ?></div>
+                    <div class="gênero">Gênero: <?= $filmes->genero ?></div>
+                    <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
+                    <div class="avaliacao">Avaliação: <?= $filmes->avaliacao ?></div>
                 </div>
             </div>
         </div>
@@ -59,11 +61,11 @@
                     <img src="src/public/img/got.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Games of Thrones</div>
-                    <div class="ano">Ano: 2011-2019</div>
-                    <div class="gênero">Gênero: Drama, Fantasia, Ação</div>
-                    <div class="elenco">Elenco: Emilia Clarke, Kit Harington, Lena Headey, Sophie Turner, Peter Dinklage</div>
-                    <div class="avaliacao">Avaliação: 4.8</div>
+                    <div class="titulo">Título: <?= $serie->titulo ?></div>
+                    <div class="ano">Ano: <?= $serie->ano ?></div>
+                    <div class="gênero">Gênero: <?= $serie->genero ?></div>
+                    <div class="elenco">Elenco: <?= $serie->elenco ?></div>
+                    <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
                 </div>
             </div>
             <div class="series-quad">
@@ -71,11 +73,11 @@
                     <img src="src/public/img/brkbad.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Breaking Bad</div>
-                    <div class="ano">Ano: 2008-2013</div>
-                    <div class="gênero">Gênero: Drama, Ação</div>
-                    <div class="elenco">Elenco: Bryan Cranston, Anna Gunn, Aaron Paul</div>
-                    <div class="avaliacao">Avaliação: 4.8</div>
+                    <div class="titulo">Título: <?= $serie->titulo ?></div>
+                    <div class="ano">Ano: <?= $serie->ano ?></div>
+                    <div class="gênero">Gênero: <?= $serie->genero ?></div>
+                    <div class="elenco">Elenco: <?= $serie->elenco ?></div>
+                    <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
                 </div>
             </div>
             <div class="series-quad">
@@ -83,11 +85,11 @@
                     <img src="src/public/img/spn.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Supernatural</div>
-                    <div class="ano">Ano: 2005-2020</div>
-                    <div class="gênero">Gênero: Drama, Terror, Fantasia</div>
-                    <div class="elenco">Elenco: Jared Padalecki, Jensen Ackles, Misha Collins</div>
-                    <div class="avaliacao">Avaliação: 4.7</div>
+                    <div class="titulo">Título: <?= $serie->titulo ?></div>
+                    <div class="ano">Ano: <?= $serie->ano ?></div>
+                    <div class="gênero">Gênero: <?= $serie->genero ?></div>
+                    <div class="elenco">Elenco: <?= $serie->elenco ?></div>
+                    <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
                 </div>
             </div>
         </div>
@@ -97,12 +99,12 @@
                     <img src="src/public/img/irobo.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: Eu, Robô</div>
-                    <div class="ano">Ano: 2014</div>
-                    <div class="gênero">Gênero: Ficção científica</div>
-                    <div class="editora"> Editora: Editora Aleph</div>
-                    <div class="autor">Autor: Isaac Asimov</div>
-                    <div class="avaliacao">Avaliação: 4.7</div>
+                    <div class="titulo">Título: <?= $livro->titulo ?></div>
+                    <div class="ano">Ano: <?= $livro->ano ?></div>
+                    <div class="gênero">Gênero: <?= $livro->genero ?></div>
+                    <div class="editora"> Editora: <?= $livro->editora ?></div>
+                    <div class="autor">Autor: <?= $livro->autor ?></div>
+                    <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
                 </div>
             </div>
             <div class="books-quad">
@@ -110,12 +112,12 @@
                     <img src="src/public/img/codavinci.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: O Código Da Vinci</div>
-                    <div class="ano">Ano: 2004</div>
-                    <div class="gênero">Gênero: Ação e Aventura</div>
-                    <div class="editora"> Editora: Editora Arqueiro</div>
-                    <div class="autor">Autor: Dan Brown</div>
-                    <div class="avaliacao">Avaliação: 4.8</div>
+                    <div class="titulo">Título: <?= $livro->titulo ?></div>
+                    <div class="ano">Ano: <?= $livro->ano ?></div>
+                    <div class="gênero">Gênero: <?= $livro->genero ?></div>
+                    <div class="editora"> Editora: <?= $livro->editora ?></div>
+                    <div class="autor">Autor: <?= $livro->autor ?></div>
+                    <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
                 </div>
             </div>
             <div class="books-quad">
@@ -123,15 +125,16 @@
                     <img src="src/public/img/cortico.jpg" />
                 </div>
                 <div class="txt">
-                    <div class="titulo">Título: O Cortiço</div>
-                    <div class="ano">Ano: 1890</div>
-                    <div class="gênero">Gênero: Romance</div>
-                    <div class="editora"> Editora: B. L. Garnier</div>
-                    <div class="autor">Autor: Aluísio Azevedo</div>
-                    <div class="avaliacao">Avaliação: 4.7</div>
+                    <div class="titulo">Título: <?= $livro->titulo ?></div>
+                    <div class="ano">Ano: <?= $livro->ano ?></div>
+                    <div class="gênero">Gênero: <?= $livro->genero ?></div>
+                    <div class="editora"> Editora: <?= $livro->editora ?></div>
+                    <div class="autor">Autor: <?= $livro->autor ?></div>
+                    <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
