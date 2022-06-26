e471a716ee034e538c86df2f4b83b305116d9414
igorradiao <91671264+igorradiao@users.noreply.github.com>
2022-06-26 17:23:18 -0300

    Update kanban-usuario.php

--- src/views/kanban-usuario.php
+++ src/views/kanban-usuario.php
@@ -7,9 +7,6 @@
     <title>Kanban Minha Critica</title>
     <link rel="stylesheet" href="../public/css/navbar.css">
     <link rel="stylesheet" href="../public/css/kanban.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 <body>
     <nav>
@@ -18,49 +15,58 @@
     <br>
     <div class="texto-destaque">SEU KANBAN DE CRÍTICAS</div>
     <div class="table-kanban-filmes">
-      <div class="table-critica-destaque">
+    <ul style="list-style: none;">
+      <li class="table-critica-destaque">
           <img class="img" src="../public/img/interstellar.png">
-          <div class="titulo-critica-destaque">Interestelar</div>
+          <div class="titulo-critica-destaque"><strong>Interestelar</strong></div>
           <div class="texto-descritivo">Data da publicação: dd/mm/aaaa <br>
             <strong>Descrição:</strong> Este é um teste de descrição que o usuario podera
             fazer acerca do filme escolhido.</div>
-            <span class="material-symbols-outlined">
-                edit</span>
-            <span class="material-symbols-outlined">
-                visibility</span>
-            <span class="material-symbols-outlined">
-                delete</span>
-      </div>
+            </li>
+      <li class="table-critica-destaque">
+        <img class="img" src="../public/img/interstellar.png">
+        <div class="titulo-critica-destaque">Interestelar</div>
+        <div class="texto-descritivo">Data da publicação: dd/mm/aaaa <br>
+          <strong>Descrição:</strong> Este é um teste de descrição que o usuario podera
+          fazer acerca do filme escolhido.</div>
+        </li>
+    <li class="table-critica-destaque">
+        <img class="img" src="../public/img/interstellar.png">
+        <div class="titulo-critica-destaque">Interestelar</div>
+        <div class="texto-descritivo">Data da publicação: dd/mm/aaaa <br>
+          <strong>Descrição:</strong> Este é um teste de descrição que o usuario podera
+          fazer acerca do filme escolhido.</div>
+    </li>
+    <li class="table-critica-destaque">
+        <img class="img" src="../public/img/interstellar.png">
+        <div class="titulo-critica-destaque">Interestelar</div>
+        <div class="texto-descritivo">Data da publicação: dd/mm/aaaa <br>
+          <strong>Descrição:</strong> Este é um teste de descrição que o usuario podera
+          fazer acerca do filme escolhido.</div>
+    </li>
+    </ul>
     </div>
     <div class="table-kanban-series">
-        <div class="table-critica-destaque">
+        <ul style="list-style: none;">
+        <li class="table-critica-destaque">
             <img class="img" src="../public/img/got.png">
             <div class="titulo-critica-destaque">Game Of Thrones</div>
             <div class="texto-descritivo">Data da publicação: dd/mm/aaaa <br>
               <strong>Descrição:</strong> Este é um teste de descrição que o usuario podera
               fazer acerca da serie escolhida.</div>
-              <span class="material-symbols-outlined">
-                  edit</span>
-              <span class="material-symbols-outlined">
-                  visibility</span>
-              <span class="material-symbols-outlined">
-                  delete</span>
         </div>
-    </div>
+    </li>
+    </ul>
   <div class="table-kanban-livros">
-    <div class="table-critica-destaque">
+    <ul style="list-style: none;">
+    <li class="table-critica-destaque">
         <img class="img" src="../public/img/1984.png">
         <div class="titulo-critica-destaque">1984</div>
         <div class="texto-descritivo">Data da publicação: dd/mm/aaaa <br>
           <strong>Descrição:</strong> Este é um teste de descrição que o usuario podera
           fazer acerca de um livro escolhido.</div>
-          <span class="material-symbols-outlined">
-              edit</span>
-          <span class="material-symbols-outlined">
-              visibility</span>
-          <span class="material-symbols-outlined">
-              delete</span>
-    </div>
+    </li>
+    </ul>
   </div>
 </body>
 </html>
