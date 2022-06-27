962a45fed289b13d45fe1c79ce0c45b7b4fe2463
Thiago Bauce <thiedbauce@gmail.com>
2022-06-26 20:12:53 -0400

    link com review para inserir critica

--- index.php
+++ index.php
@@ -16,119 +16,137 @@
     <div class="corpo">
         <div class="movies">
             <div class="movies-quad">
-                <div class="img">
-                    <img src=<?= $filmes->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $filmes->titulo ?></div>
-                    <div class="ano">Ano: <?= $filmes->ano ?></div>
-                    <div class="duracao">Duração: <?= $filmes->duracao ?></div>
-                    <div class="gênero">Gênero: <?= $filmes->genero ?></div>
-                    <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<?= $filmes->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $filmes->titulo ?></div>
+                        <div class="ano">Ano: <?= $filmes->ano ?></div>
+                        <div class="duracao">Duração: <?= $filmes->duracao ?></div>
+                        <div class="gênero">Gênero: <?= $filmes->genero ?></div>
+                        <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
+                    </div>
+                </a>
             </div>
             <div class="movies-quad">
-                <div class="img">
-                    <img src=<? $filmes->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $filmes->titulo ?></div>
-                    <div class="ano">Ano: <?= $filmes->ano ?></div>
-                    <div class="duracao">Duração: <?= $filmes->duracao ?></div>
-                    <div class="gênero">Gênero: <?= $filmes->genero ?></div>
-                    <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $filmes->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $filmes->titulo ?></div>
+                        <div class="ano">Ano: <?= $filmes->ano ?></div>
+                        <div class="duracao">Duração: <?= $filmes->duracao ?></div>
+                        <div class="gênero">Gênero: <?= $filmes->genero ?></div>
+                        <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
+                    </div>
+                </a>
             </div>
             <div class="movies-quad">
-                <div class="img">
-                    <img src=<? $filmes->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $filmes->titulo ?></div>
-                    <div class="ano">Ano: <?= $filmes->ano ?></div>
-                    <div class="duracao">Duração: <?= $filmes->duracao ?></div>
-                    <div class="gênero">Gênero: <?= $filmes->genero ?></div>
-                    <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $filmes->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $filmes->titulo ?></div>
+                        <div class="ano">Ano: <?= $filmes->ano ?></div>
+                        <div class="duracao">Duração: <?= $filmes->duracao ?></div>
+                        <div class="gênero">Gênero: <?= $filmes->genero ?></div>
+                        <div class="elenco">Elenco: <?= $filmes->elenco ?></div>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="series">
             <div class="series-quad">
-                <div class="img">
-                    <img src=<? $serie->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $serie->titulo ?></div>
-                    <div class="ano">Ano: <?= $serie->ano ?></div>
-                    <div class="gênero">Gênero: <?= $serie->genero ?></div>
-                    <div class="elenco">Elenco: <?= $serie->elenco ?></div>
-                    <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $serie->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $serie->titulo ?></div>
+                        <div class="ano">Ano: <?= $serie->ano ?></div>
+                        <div class="gênero">Gênero: <?= $serie->genero ?></div>
+                        <div class="elenco">Elenco: <?= $serie->elenco ?></div>
+                        <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
+                    </div>
+                </a>
             </div>
             <div class="series-quad">
-                <div class="img">
-                    <img src=<? $serie->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $serie->titulo ?></div>
-                    <div class="ano">Ano: <?= $serie->ano ?></div>
-                    <div class="gênero">Gênero: <?= $serie->genero ?></div>
-                    <div class="elenco">Elenco: <?= $serie->elenco ?></div>
-                    <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $serie->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $serie->titulo ?></div>
+                        <div class="ano">Ano: <?= $serie->ano ?></div>
+                        <div class="gênero">Gênero: <?= $serie->genero ?></div>
+                        <div class="elenco">Elenco: <?= $serie->elenco ?></div>
+                        <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
+                    </div>
+                </a>
             </div>
             <div class="series-quad">
-                <div class="img">
-                    <img src=<? $serie->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $serie->titulo ?></div>
-                    <div class="ano">Ano: <?= $serie->ano ?></div>
-                    <div class="gênero">Gênero: <?= $serie->genero ?></div>
-                    <div class="elenco">Elenco: <?= $serie->elenco ?></div>
-                    <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $serie->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $serie->titulo ?></div>
+                        <div class="ano">Ano: <?= $serie->ano ?></div>
+                        <div class="gênero">Gênero: <?= $serie->genero ?></div>
+                        <div class="elenco">Elenco: <?= $serie->elenco ?></div>
+                        <div class="avaliacao">Avaliação: <?= $serie->avaliacao ?></div>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="books">
             <div class="books-quad">
-                <div class="img">
-                    <img src=<? $livro->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $livro->titulo ?></div>
-                    <div class="ano">Ano: <?= $livro->ano ?></div>
-                    <div class="gênero">Gênero: <?= $livro->genero ?></div>
-                    <div class="editora"> Editora: <?= $livro->editora ?></div>
-                    <div class="autor">Autor: <?= $livro->autor ?></div>
-                    <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $livro->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $livro->titulo ?></div>
+                        <div class="ano">Ano: <?= $livro->ano ?></div>
+                        <div class="gênero">Gênero: <?= $livro->genero ?></div>
+                        <div class="editora"> Editora: <?= $livro->editora ?></div>
+                        <div class="autor">Autor: <?= $livro->autor ?></div>
+                        <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
+                    </div>
+                </a>
             </div>
             <div class="books-quad">
-                <div class="img">
-                    <img src=<? $livro->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $livro->titulo ?></div>
-                    <div class="ano">Ano: <?= $livro->ano ?></div>
-                    <div class="gênero">Gênero: <?= $livro->genero ?></div>
-                    <div class="editora"> Editora: <?= $livro->editora ?></div>
-                    <div class="autor">Autor: <?= $livro->autor ?></div>
-                    <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $livro->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $livro->titulo ?></div>
+                        <div class="ano">Ano: <?= $livro->ano ?></div>
+                        <div class="gênero">Gênero: <?= $livro->genero ?></div>
+                        <div class="editora"> Editora: <?= $livro->editora ?></div>
+                        <div class="autor">Autor: <?= $livro->autor ?></div>
+                        <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
+                    </div>
+                </a>
             </div>
             <div class="books-quad">
-                <div class="img">
-                    <img src=<? $livro->caminhoimg ?> />
-                </div>
-                <div class="txt">
-                    <div class="titulo">Título: <?= $livro->titulo ?></div>
-                    <div class="ano">Ano: <?= $livro->ano ?></div>
-                    <div class="gênero">Gênero: <?= $livro->genero ?></div>
-                    <div class="editora"> Editora: <?= $livro->editora ?></div>
-                    <div class="autor">Autor: <?= $livro->autor ?></div>
-                    <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
-                </div>
+                <a href="src\views\movie_search_page.php" target="_parent" class="menu__item">
+                    <div class="img">
+                        <img src=<? $livro->caminhoimg ?> />
+                    </div>
+                    <div class="txt">
+                        <div class="titulo">Título: <?= $livro->titulo ?></div>
+                        <div class="ano">Ano: <?= $livro->ano ?></div>
+                        <div class="gênero">Gênero: <?= $livro->genero ?></div>
+                        <div class="editora"> Editora: <?= $livro->editora ?></div>
+                        <div class="autor">Autor: <?= $livro->autor ?></div>
+                        <div class="avaliacao">Avaliação: <?= $livro->avaliacao ?></div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
